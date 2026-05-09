bcc83f5f9eae073fdbfd0a48e591ab97c89aab83
Armaghan Azarbarzin <armaghan.azarbarzin@gmail.com>
2026-05-09 21:30:08 +0000

    delete untitles_2 fle

--- models/staging/Untitled-2.sql
+++
@@ -1,9 +0,0 @@
-{% set tabletype="orders" %}
-{% set category=["Furniture", "Office", "Technology"] %}
-
-select 
-orderid,
-case when category = '{{category}}' then orderprofit end as {{category}}_orderprofit
-
-from {{ ref('stg_orders') }}
-
